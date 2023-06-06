c8b548506802754f4dffeff45cc8c1264fb6749a
Mooizz  Abdul <albusdumbledore8918@gmail.com>
2023-06-06 21:28:53 +0530

    Add basepath

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
 
-module.exports = nextConfig
+  // Add basePath
+  basePath: "/home",
+};
+
+module.exports = nextConfig;
